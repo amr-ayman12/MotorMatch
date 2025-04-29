65d67956aa92ab8f88e7c24c602b3c1e53f3e686
Amr M.Ayman <amr86967p@gmail.com>
2025-04-29 04:34:01 +0300

    Add files via upload

---
+++ CarBrands.js
@@ -0,0 +1,112 @@
+function bmwm5(){
+window.location.href ="BMW_M5_CS.html";
+}
+function bmwm4(){
+window.location.href ="BMW_M4.html";
+}
+function bmwi8(){
+window.location.href ="";
+}
+function phantom(){
+window.location.href ="";
+}
+function ghost(){
+window.location.href ="";
+}
+function f8(){
+window.location.href ="Ferrari_f8.html";
+}
+function roma(){
+window.location.href ="";
+}
+function cla(){
+window.location.href ="Mercedes _CLA.html";
+}
+function cle53(){
+window.location.href ="Mercedes_AMG_CLE_53.html";
+}
+function e53(){
+window.location.href ="Mercedes_E_53.html";
+}
+function gt43(){
+window.location.href ="Mercedes_GT_43.html";
+}
+function gt55(){
+window.location.href ="Mercedes_GT_55.html";
+}
+function maybach(){
+window.location.href ="Mercedes_Maybach.html";
+}
+function elise(){
+window.location.href ="Lotus _Eslie.html";
+}
+function evora(){
+window.location.href ="Lotus_Evora.html";
+}
+function exige(){
+window.location.href ="Lotus_Exige.html";
+}
+function mc20(){
+window.location.href ="";
+}
+function gracabrio(){
+window.location.href ="";
+}
+function granturismo(){
+window.location.href ="";
+}
+function grecale(){
+window.location.href ="";
+}
+function RollsRoyceCullinan(){
+window.location.href ="Rolls_Royce.html";
+}
+function MercedesGClass63(){
+window.location.href ="Mercedes G-Class 63.html";
+}
+function BentleyContinentalGT(){
+window.location.href ="Bentley.html";
+}
+// function (){
+// window.location.href ="";
+// }
+// function (){
+// window.location.href ="";
+// }
+// function (){
+// window.location.href ="";
+// }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cars = document.querySelectorAll('.car');
+  
+    function handleScroll() {
+      cars.forEach(car => {
+        const carRect = car.getBoundingClientRect();
+        const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        const isVisible = carRect.top < windowHeight * 0.8 && carRect.bottom > windowHeight * 0.2;
+        const carCenter = carRect.left + carRect.width / 2;
+  
+        if (isVisible && !car.classList.contains('appeared')) {
+          car.classList.add('appeared');
+  
+          if (carCenter > windowWidth / 2) {
+            car.classList.add('appear-right');
+          } else if (carCenter < windowWidth / 2) {
+            car.classList.add('appear-left');
+          } else {
+            car.classList.add('appear-bottom');
+          }
+        }
+      });
+    }
+  
+    // Listen for the scroll event
+    window.addEventListener('scroll', handleScroll);
+  
+    // Trigger handleScroll on initial load and resize
+    handleScroll();
+    window.addEventListener('resize', handleScroll);
+  });
